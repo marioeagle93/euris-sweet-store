93c9dbb3577c6abad09eadf3ab58fbef29df9029
Mario Battaglia <mario.battaglia@nttdata.com>
2023-06-01 11:20:43 +0200

    Collegamento API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DefaultService } from './api/api/default.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'euris-sweet-store';
+  idStore = 'ijpxNJLM732vm8AeajMR';
+
+  constructor(private defaultService: DefaultService) {}
+
+  ngOnInit(): void {
+    this.defaultService.storesIdStoreGet(this.idStore).subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
